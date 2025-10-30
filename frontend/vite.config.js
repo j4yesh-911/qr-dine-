30e0a30a871d346aa8a528fcba2c5625778a729a
j4yesh-911 <jayeshgayri12@gmail.com>
2025-10-30 10:39:23 +0530

    fix signup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-const API_TARGET = process.env.VITE_API_URL || "http://localhost:4000"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    // This block tells Vite to redirect API calls to your backend server
-    host : true,
-    proxy: {
-      '/api': {
-        target: API_TARGET, // <--- Your running Node.js server port
-        changeOrigin: true,
-        secure: false, 
-      },
-    },
-  },
-
 })
