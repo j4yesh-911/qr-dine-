cfa349697da9d5157a7fb06b81cd2dbf4073e3da
j4yesh-911 <jayeshgayri12@gmail.com>
2025-11-02 13:55:56 +0530

    vercel update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Default for Vercel
+  },
+  server: {
+    port: 5173,
+  },
+  // ✅ Important for React Router (SPA)
+  preview: {
+    port: 4173,
+  },
 })
